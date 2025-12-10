10d3ef181827501e2d23d5b91ef6aa32e7d16639
K-lubna <164841387+K-lubna@users.noreply.github.com>
2025-12-10 22:16:22 +0530

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -30,7 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             try {
-                const response = await fetch('http://localhost:3000/api/login', {
+                const response = await fetch('https://broker-view-live.onrender.com/api/login', { 
+
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email })
